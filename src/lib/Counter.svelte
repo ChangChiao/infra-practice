e5ff056b1db870b7443ea8fdf8237c20451fc0cf
JoeCT_Chang <JoeCT.Chang@moxa.com>
2023-09-08 15:46:46 +0800

    feat: add minus function

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
-  let count: number = 0
+  let count: number = 0;
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
+  const minus = () => {
+    count -= 1;
+  };
 </script>
 
-<button on:click={increment}>
+<button on:click={increment}> add </button>
+<h3>
   count is {count}
-</button>
+</h3>
+<button on:click={minus}>minus</button>
